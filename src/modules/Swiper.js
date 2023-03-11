a09b628ca08f6055135151e0700b1a3cccbc153f
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-11 08:48:11 +0400

    sd

--- src/modules/Swiper.js
+++ src/modules/Swiper.js
@@ -2,5 +2,20 @@ import jqueryScrollify from "jquery-scrollify";
 jqueryScrollify(function () {
   $.scrollify({
     section: ".layout__list-item",
+    sectionName: "section-name",
+    interstitialSection: "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset: 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll: true,
+    before: function () {},
+    after: function () {},
+    afterResize: function () {},
+    afterRender: function () {},
   });
 });
