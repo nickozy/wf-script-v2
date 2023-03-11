15917841532c3e1c03a73a895c9aff3e3a938157
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-11 07:56:22 +0400

    we

--- src/modules/Swiper.js
+++ src/modules/Swiper.js
@@ -1,20 +1,20 @@
 import { module } from 'modujs';
 import Swiper from "swiper";
 
-function lau(){
-    if(window.innerWidth < 992){
-        const layutSwiper = new Swiper('.layout', {
-            speed: 200,
-            slideClass:  'layout__list-item',
-            wrapperClass: 'layout__list'
-        })
-    }
-
-}
 
 export default class extends module {
     constructor(m) {
         super(m);
     }
-    lau()
+
+    init(){
+        const sswiper = new Swiper('.layout', {
+        speed: 200,
+        slideClass:  'layout__list-item',
+        wrapperClass: 'layout__list'
+    })
+    
+
+}
+        
 }
