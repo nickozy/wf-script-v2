13e9a1e9e945525b9ec9e234ce533c2af2beefcb
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-11 08:24:42 +0400

    we

--- src/modules/Swiper.js
+++ src/modules/Swiper.js
@@ -1,16 +1,7 @@
-import { module } from 'modujs';
-import Swiper from 'swiper';
+import Swiper from "swiper";
 
-export default class extends module {
-    constructor(m) {
-        super(m);
-    }
-
-    init() {
-        let sswiper = new Swiper('.layout', {
-            direction: 'vertical',
-            wrapperClass: 'layout__list',
-            slideClass:  'layout__list-item'
-        })
-    }
-}
+let sswiper = new Swiper(".layout", {
+  direction: "vertical",
+  wrapperClass: "layout__list",
+  slideClass: "layout__list-item",
+});
