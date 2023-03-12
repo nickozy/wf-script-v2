d92c35912fe6b61e77a760cd7e64ee3992880c56
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-13 03:11:50 +0400

    we

--- src/modules.js
+++ src/modules.js
@@ -1,3 +1,3 @@
 export { default as Load } from "./modules/Load";
 export { default as Scroll } from "./modules/Scroll";
-export { default as Acc } from "./modules/Acc";
+export { default as Accordion } from "./modules/Accordion";
