220b8a2ef31a3251dfbbdcf227bd399f747d6f1b
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-13 02:58:01 +0400

    acc

--- src/modules.js
+++ src/modules.js
@@ -1,2 +1,3 @@
 export { default as Load } from "./modules/Load";
 export { default as Scroll } from "./modules/Scroll";
+export { default as Acc } from "./modules/Acc";
---
+++ src/modules/Acc.js
@@ -0,0 +1,33 @@
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+ 
+        this.events = {
+            click: {
+                header: 'toggleSection'
+            }
+        }
+    }
+ 
+    init() {
+        if (this.data('open')) {
+            this.$('section')[0].classList.add('is-open');
+        }
+    }
+ 
+    toggleSection(e) {
+        const target = e.currentTarget;
+        const section = this.parent('section', target);
+        const main = this.$('main', target);
+        
+        if (section.classList.contains('is-open')) {
+            section.classList.remove('is-open');
+        } else {
+            this.$('section.is-open').classList.remove('is-open');
+            section.classList.add('is-open');
+            this.call('scrollto', section, 'scroll', 'main');
+        }
+    }
+}
