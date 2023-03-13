1f12e23b04a007c2b75e7360e1f919cd46aaa661
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-13 05:20:48 +0400

    asd

--- src/modules/Accordion.js
+++ src/modules/Accordion.js
@@ -4,26 +4,21 @@ import LocomotiveScroll from 'locomotive-scroll';
 export default class extends module {
     constructor(m) {
         super(m);
- 
         this.events = {
             click: {
                 header: 'toggleSection'
             }
-        }
-    }
- 
-    init(){
-
-        this.scroll = new LocomotiveScroll();
+        } 
     }
  
     toggleSection(e) {
         const target = e.currentTarget;
         const section = this.parent('section', target);
         const main = this.$('main', target);
-        this.scroll.update();
+        
 
-
+ const scr = new LocomotiveScroll();
+scr.update();
         if (section.classList.contains('is-open')) {
             section.classList.remove('is-open');
         } else {
